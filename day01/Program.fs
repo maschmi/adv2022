0530109561470026a611cdb22b3f95c84f3a6c30
Martin Schmidt <inw@immernurwollen.de>
2022-12-09 20:05:12 +0100

    day01

---
+++ day01/Program.fs
@@ -0,0 +1,51 @@
+﻿open System
+
+open System.Diagnostics
+open Microsoft.FSharp.Core
+open utils
+let inFile = "day01/input-01.txt" 
+
+
+let nextSlice input  =
+    input
+    |> List.findIndex (fun item -> item.Equals(""))
+    |> fun x -> input[..x]
+    
+let splitBy v list =
+    let sumIt list = 
+      if list = [] then []
+      else [List.sum list]
+    
+    let rec loop groupSoFar list = seq { 
+      match list with
+      | [] -> yield! sumIt groupSoFar
+      | head::tail when head = v ->
+          yield! sumIt groupSoFar
+          yield! loop [] tail
+      | head::tail ->
+          yield! loop (groupSoFar @ [head |> int]) tail }
+    loop [] list |> List.ofSeq
+    
+let findMax list =
+    list
+    |> Seq.mapi (fun i v -> i, v)
+    |> Seq.maxBy snd
+
+let printMax file = 
+    utils.Input.readLines file
+            |> List.ofArray
+            |> splitBy ""
+            |> findMax
+            |> printfn "Max calories %A" 
+
+let sumTop file n = 
+    utils.Input.readLines file
+            |> List.ofArray
+            |> splitBy ""
+            |> List.sortDescending
+            |> List.take n
+            |> List.sum
+            |> printfn "Sum of top %d: %d" n 
+            
+printMax inFile
+sumTop inFile 3
---
+++ utils/Library.fs
@@ -0,0 +1,21 @@
+﻿namespace utils
+
+open System.IO
+
+module Input =
+    let rootPath = "/home/martin/src/AdventOfCode2022"
+    let readLines file =
+        File.ReadAllLines $"{rootPath}/{file}"
+        
+module ListUtils =
+    let splitBy v list =
+       
+        let rec loop groupSoFar list = seq { 
+          match list with
+          | [] -> yield! groupSoFar
+          | head::tail when head = v ->
+              yield! groupSoFar
+              yield! loop [] tail
+          | head::tail ->
+              yield! loop (groupSoFar @ [head]) tail }
+        loop [] list |> List.ofSeq
